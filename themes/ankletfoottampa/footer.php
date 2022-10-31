65fe75a4c1fe25e7925f6420120ebc7f6ff54b3e
mager19 <mager19@gmail.com>
2022-10-31 15:29:29 -0500

    Footer Added

--- themes/ankletfoottampa/footer.php
+++ themes/ankletfoottampa/footer.php
@@ -8,61 +8,111 @@
  */
 ?>
 
+</div>
+<!--/ Content Page -->
+
+<?php
+if (!is_404()) {
+    if (!is_home()) {
+        get_template_part('template-parts/cta', 'footer');
+    }
+}
+?>
+
+<!-- Footer -->
+<footer class="site-footer bg-alternate-1">
+    <div class="container mx-auto py-14">
+        <div class="site__footer__content flex flex-wrap items-center md:items-start px-4 justify-center lg:justify-start">
+
+            <div class="site__footer__logo w-full lg:w-5/12 text-center lg:text-left mb-0">
+                <div class="logo__footer">
+                    <a href="<?php echo esc_url(get_bloginfo('url')); ?>">
+                        <?php
+                        $GETlogo = get_field('logo', 'option');
+                        if ($GETlogo) {
+                            fps_get_Image($GETlogo);
+                        } else {
+                            echo "<h3 class='mb-0'>Logo Brand</h3>";
+                        } ?>
+                    </a>
+                </div>
+                <?php $infoFooter = get_field('info_footer', 'option');
+                if ($infoFooter) { ?>
+                    <div class="content__footer">
+                        <?php echo $infoFooter; ?>
+                    </div>
+                <?php
+                }
+                ?>
             </div>
-            <!--/ Content Page -->
+
+            <?php $hours = get_field('hours_of_operations', 'option'); ?>
+            <div class="hours w-full md:w-1/2 lg:w-4/12 text-center lg:text-left my-4">
+                <h3 class="title title--body lg:title--6">
+                    Hours of Operations:
+                </h3>
+                <div class="hours__content">
+                    <?php echo $hours; ?>
+                </div>
+
+                <?php
+                $rows = get_field('social_network', 'option');
+                if ($rows) { ?>
+                    <div class="social__footer">
+                        <?php
+                        foreach ($rows as $row) { ?>
+                            <a href="<?php echo $row['link']; ?>">
+                                <div class="icon-<?php echo $row['icon']; ?>--d"></div>
+                            </a>
+                    <?php
+                        }
+                    }
+                    ?>
+                    </div>
+            </div>
 
             <?php
-            if (!is_404()) {
-                if (!is_home()) {
-                    get_template_part('template-parts/cta', 'footer');
-                }
-            }
-            ?>
+            $rows = get_field('locations', 'option');
+            if ($rows) { ?>
+                <div class="contact__footer w-full md:w-1/2 lg:w-3/12 text-center lg:text-left mt-4">
+                    <h3 class="title title--body lg:title--6">
+                        Contact:
+                    </h3>
 
-            <!-- Footer -->
-            <footer class="site-footer bg-gray-100">
-                <div class="container mx-auto py-14">
-                    <div class="site__footer__content flex flex-wrap px-4 justify-center lg:justify-start">
-                        
-                        <div class="site__footer__logo w-full md:w-3/12 text-center lg:text-left mb-4">
-
-                            <a href="<?php echo esc_url(get_bloginfo('url')); ?>">
-                                <?php if ($GETlogo) { 
-                                    fps_get_Image($GETlogo);
-                                } else {
-                                    echo "<h3 class='mb-0'>Logo Brand</h3>";
-                                } ?>
-                            </a>
-
-                        </div>
-
-                        <div class="site-footer__top w-full md:w-9/12">
-
-                            <!-- Footer Menu -->
-                            <div class="footer-menu">
-                                <?php if (has_nav_menu('menu-2')) {
-                                    wp_nav_menu(array('theme_location' => 'menu-2'));
-                                } ?>
+                    <div class="locations__footer">
+                        <?php
+                        foreach ($rows as $row) { ?>
+                            <div class="item__location__footer">
+                                <?php
+                                echo $row['location_information']; ?>
                             </div>
-                            <!--/Footer Menu-->
-
-                        </div>
-
-                        <div class="site-footer__bottom">
-
-                            <!-- copyright -->
-                            <div class="copyright text-center">
-                                <?php the_field('fps_copyright', 'option'); ?>
-                            </div>
-                            <!--/ copyright -->
-
-                        </div>
+                        <?php
+                        }
+                        ?>
                     </div>
                 </div>
-            </footer>
-            <!--/ Footer -->
+            <?php
+
+            } ?>
+
+
+
+
+            <div class="site-footer__bottom">
+
+                <!-- copyright -->
+                <div class="copyright text-center">
+                    <?php the_field('fps_copyright', 'option'); ?>
+                </div>
+                <!--/ copyright -->
+
+            </div>
         </div>
-    <?php wp_footer(); ?>
+    </div>
+</footer>
+<!--/ Footer -->
+</div>
+<?php wp_footer(); ?>
 </body>
 
 </html>
