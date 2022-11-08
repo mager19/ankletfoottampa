6ebb1049d193c5e05102b2535837f282ca29147a
mager19 <mager19@gmail.com>
2022-11-08 12:08:31 -0500

    modal members

--- themes/ankletfoottampa/template-parts/items/item-team-member.php
+++ themes/ankletfoottampa/template-parts/items/item-team-member.php
@@ -14,20 +14,28 @@ $modalName = $args['modalName'];
 </div>
 
 
-<div id="modal-custom-ourteam<?php echo $args['counter'];  ?>" class="modalMenu">
+<div id="modal-custom-ourteam<?php echo $args['counter'];  ?>" class="modalMenu modalTeam pb-4">
     <div class="modal__header">
         <button data-iziModal-close class="icon-close">
             <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M1 1L15 15M1 15L15 1" stroke="#A9E1D3" stroke-width="2" />
             </svg>
         </button>
-
-
     </div>
 
-    <div class="modal__content relative">
-        <h5 class="title"><?php the_title(); ?></h5>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat minima quas, minus at dicta explicabo! Sed dolor placeat veritatis consequatur vel voluptates, culpa consectetur non exercitationem. Optio, perspiciatis ipsum! Alias!
-    </div>
+    <div class="content w-full ">
+        <div class="container__info md:flex  w-full gap-4">
+            <div class="left">
+                <?php the_post_thumbnail(); ?>
 
+            </div>
+            <div class="right pb-8">
+                <h3 class="title"><?php the_title(); ?></h3>
+                <span><?php the_field('position'); ?></span>
+                <?php the_content(); ?>
+
+                <a class="btn btn--primary mb-4" href="/schedule-an-appointment/">Schedule an Appointment</a>
+            </div>
+        </div>
+    </div>
 </div>
